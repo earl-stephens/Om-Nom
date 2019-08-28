3eeeb3597445bade68d4fa94bf327c7e50454320
earl-stephens <stephensearl@hotmail.com>
2019-08-28 11:03:29 -0600

    Setup the recipes endpoint for getting recipes from the edamam microservice

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -3,22 +3,16 @@ var router = express.Router();
 var Meal = require('../../../models').Meal;
 var Food = require('../../../models').Food;
 var MealFoods = require('../../../models').MealFoods;
-var pry = require('pryjs')
+var pry = require('pryjs');
+var fetch = require('node-fetch');
 
 router.get('/food_search', function(req,res) {
   var foodType = req.query.q;
-  // fetch(`/api/v1/recipes/food_search?q=beef`, {headers: {"Accept": "application/json", "Content-Type": "application/json"}})
-  fetch(`localhost:3000/api/v1/recipes/food_search?q=${foodType}`, {headers: {"Accept": "application/json", "Content-Type": "application/json"}})
+  fetch(`https://om-nom-edamam.herokuapp.com/api/v1/recipes/food_search?q=${foodType}`, {headers: {"Accept": "application/json", "Content-Type": "application/json"}})
   .then(res => res.json())
-  // .then(function(response) {
-  //   return response.json();
-  // })
-  // .then(function(myJson) {
-  //   console.log(JSON.stringify(myJson));
-  // })
-  .then(created => {console.log(created);
+  .then(json => {
     res.setHeader("Content-Type", "application/json");
-    res.status(200).send(JSON.stringify(created));
+    res.status(200).send(JSON.stringify(json));
   })
   .catch(error => {
     res.setHeader("Content-Type", "application/json");
