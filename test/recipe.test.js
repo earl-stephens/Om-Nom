d0b8bbb73768070d5f803319c8c52531b0301e04
earl-stephens <stephensearl@hotmail.com>
2019-08-28 16:23:37 -0600

    Set up the endpoint to get recipes sorted by time based on food type

--- test/recipe.test.js
+++ test/recipe.test.js
@@ -5,7 +5,7 @@ var express = require('express');
 var test = express();
 var Food = require("../models").Food
 
-describe('recipe requests', () =>{
+describe('recipe requests', () => {
   it('GET recipes for a food type already in the database', () => {
     return request(app)
     .get('/api/v1/recipes/food_search?q=beef')
@@ -24,10 +24,29 @@ describe('recipe requests', () =>{
   });
 });
 
-describe('recipe calories request', () =>{
+describe('recipe calories request', () => {
   it('GET recipes based on number of calories', () => {
     return request(app)
     .get('/api/v1/recipes/calorie_search?q=150-300')
+    .set("Content-Type", "application/json")
+    .set("Accept", "application/json")
+    .then(response => {
+      expect(Object.keys(response.body[0])).toContain('title')
+      expect(Object.keys(response.body[0])).toContain('cookTime')
+      expect(Object.keys(response.body[0])).toContain('caloriesPerServing')
+      expect(Object.keys(response.body[0])).toContain('servingAmount')
+      expect(Object.keys(response.body[0])).toContain('image')
+      expect(Object.keys(response.body[0])).toContain('url')
+      expect(Object.keys(response.body[0])).toContain('healthDetails')
+      expect(Object.keys(response.body[0])).toContain('ingredients')
+    })
+  });
+});
+
+describe('recipe sorted time_sort request', () => {
+  it('GET recipes sorted by preparation time', () => {
+    return request(app)
+    .get('/api/v1/recipes/time_sort?q=chocolate')
     .set("Content-Type", "application/json")
     .set("Accept", "application/json")
     .then(response => {
